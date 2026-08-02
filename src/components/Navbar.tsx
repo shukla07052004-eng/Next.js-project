57b4ac9ed83d8678b906ac3488fea2e4ea4c890d
shukla07052004-eng <shukla07052004@gmail.com>
2026-08-02 10:35:36 +0530

    identifying next.js hydration bug also fixing empty ref()

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/utils";
 
--- src/components/ui/moving-border.tsx
+++ src/components/ui/moving-border.tsx
@@ -56,7 +56,7 @@ export function Button({
 
       <div
         className={cn(
-          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.8] text-sm text-white antialiased backdrop-blur-xl ",
+          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.3] text-sm text-white antialiased backdrop-blur-xl ",
           className,
         )}
         style={{
@@ -82,7 +82,7 @@ export const MovingBorder = ({
   ry?: string;
   [key: string]: any;
 }) => {
-  const pathRef = useRef<any>();
+  const pathRef = useRef<any>(null);
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
