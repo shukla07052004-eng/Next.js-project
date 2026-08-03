0fe3c029bea29b07381677dc2f9e7afab1a5d3b4
shukla07052004-eng <shukla07052004@gmail.com>
2026-08-03 10:37:03 +0530

    extra feature of moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Homepage from "@/components/Homepage";
 import {CardHoverEffectDemo} from "@/components/cardsSection"
+import { StickyScrollRevealDemo } from "@/components/sticyScroll"
 export default function Home() {
   return (
    <>
          <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
            <Homepage/>
            <CardHoverEffectDemo/>
+           <StickyScrollRevealDemo/>
          </main>
    </>
   );
